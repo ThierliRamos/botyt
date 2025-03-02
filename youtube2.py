f6ec8f0154ebc3fd77ab3496b42fc29f6e37e211
Thierli Ramos <102276704+ThierliRamos@users.noreply.github.com>
2025-03-02 14:01:41 -0300

    Update youtube2.py

--- youtube2.py
+++ youtube2.py
@@ -1,14 +1,16 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, send_file
 import yt_dlp
 import os
 from threading import Thread
+import time
+from urllib.parse import urlparse, parse_qs
 
 # Criação da Blueprint
 youtube2_app = Blueprint('youtube2', __name__)
 
 download_progress = 0
 
-def download_video(url):
+def download_video(url, output_file, format):
     global download_progress
     download_progress = 0
 
@@ -19,16 +21,17 @@ def download_video(url):
         elif d['status'] == 'finished':
             download_progress = 100
 
-    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-
     options = {
-        'format': 'best',
-        'outtmpl': os.path.join(downloads_path, '%(title)s.%(ext)s'),
+        'format': format,
+        'outtmpl': output_file,
         'progress_hooks': [progress_hook],
     }
 
-    with yt_dlp.YoutubeDL(options) as ydl:
-        ydl.download([url])
+    try:
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download([url])
+    except Exception as e:
+        print(f"Erro: {e}")
 
 @youtube2_app.route('/')
 def index():
@@ -37,11 +40,24 @@ def index():
 @youtube2_app.route('/download_video', methods=['POST'])
 def download_video_route():
     url = request.form['url']
+    format = request.form['format']  # Obtém o formato enviado pelo formulário
+
     # Validação da URL
     if "youtube.com" not in url and "youtu.be" not in url:
         return jsonify({'status': 'error', 'message': 'URL inválida!'}), 400
-    thread = Thread(target=download_video, args=(url,))
+    
+    # Configuração do caminho de download
+    parsed_url = urlparse(url)
+    video_id = parse_qs(parsed_url.query).get('v', [None])[0]
+    if not video_id:
+        return jsonify({'status': 'error', 'message': 'ID do vídeo não encontrado!'}), 400
+
+    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+    output_file_path = os.path.join(downloads_path, f"{video_id}.mp4")  # Altere a extensão conforme necessário
+
+    thread = Thread(target=download_video, args=(url, output_file_path, format))
     thread.start()
+
     return jsonify({'status': 'success'})
 
 @youtube2_app.route('/progress', methods=['GET'])
