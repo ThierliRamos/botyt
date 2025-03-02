7e0fdb943c60569e7038c783f5e1e03e236c8940
Thierli Ramos <102276704+ThierliRamos@users.noreply.github.com>
2025-03-02 12:36:34 -0300

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, send_file
 import yt_dlp
 import os
 from threading import Thread
+import time
 
 # Criação da Blueprint
 youtube_app = Blueprint('youtube', __name__)
 
 download_progress = 0
 
-def download_audio(url):
+def download_audio(url, output_file):
     global download_progress
     download_progress = 0
 
@@ -19,13 +20,11 @@ def download_audio(url):
         elif d['status'] == 'finished':
             download_progress = 100
 
-    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-
     options = {
         'format': 'bestaudio/best',
         'extractaudio': True,
         'audioformat': 'mp3',
-        'outtmpl': os.path.join(downloads_path, '%(title)s.%(ext)s'),
+        'outtmpl': output_file,
         'progress_hooks': [progress_hook],
     }
 
@@ -39,12 +38,25 @@ def index():
 @youtube_app.route('/download_audio', methods=['POST'])
 def download_audio_route():
     url = request.form['url']
+    
     # Validação da URL
     if "youtube.com" not in url and "youtu.be" not in url:
         return jsonify({'status': 'error', 'message': 'URL inválida!'}), 400
-    thread = Thread(target=download_audio, args=(url,))
+    
+    # Nome do arquivo de saída
+    title = url.split('=')[1]  # Extraia o ID do vídeo ou faça uma lógica para obter o título
+    output_file_path = os.path.join(os.path.expanduser("~"), "Downloads", f"{title}.mp3")
+    
+    # Inicie o download em uma thread
+    thread = Thread(target=download_audio, args=(url, output_file_path))
     thread.start()
-    return jsonify({'status': 'success'})
+
+    # Aguarde o download concluir antes de enviar o arquivo
+    while not os.path.exists(output_file_path):
+        time.sleep(1)  # Espera que o arquivo seja criado
+
+    # Retornar o arquivo para download
+    return send_file(output_file_path, as_attachment=True)
 
 @youtube_app.route('/progress', methods=['GET'])
 def progress():
